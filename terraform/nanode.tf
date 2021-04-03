3aa2e61ac30c34bd234f072b1478ba01dbca3581
bluebrown <nico-braun@live.de>
2021-04-03 11:36:40 +0000

    fix dockerd-exporter

--- terraform/nanode.tf
+++ terraform/nanode.tf
@@ -12,27 +12,16 @@ provider "linode" {
 
 data "linode_profile" "me" {}
 
-resource "linode_instance" "playbox" {
-  label            = "box-1"
+resource "linode_instance" "swarm-node" {
+  count            = 2
+  label            = "swarm-node-${count.index}"
   image            = "linode/ubuntu20.04"
   type             = "g6-nanode-1"
   region           = "eu-west"
-  tags             = ["terraform", "playbox", "worker"]
-  group            = "playbox-cluster"
+  tags             = ["terraform", "ansible", "docker", "swarm"]
+  group            = "swarm-cluster"
   private_ip       = true
   root_pass        = var.NODE_ROOT_PWD
   authorized_keys  = [trimspace(file(var.PUBLIC_KEY_PATH))]
   authorized_users = [data.linode_profile.me.username]
-}
-
-output "public_ip" {
-  value = linode_instance.playbox.ip_address
-}
-
-output "private_ip" {
-  value = linode_instance.playbox.private_ip_address
-}
-
-output "status" {
-  value = linode_instance.playbox.status
 }
