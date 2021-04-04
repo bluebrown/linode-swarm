46eaf24f8753d28d56167ad29012c3ed9a62e067
bluebrown <nico-braun@live.de>
2021-04-04 09:56:36 +0000

    add dns roundrobin setup to terraform

--- terraform/swarm.tf
+++ terraform/swarm.tf
@@ -10,18 +10,55 @@ provider "linode" {
   token = var.LINODE_TOKEN
 }
 
+# get profile data
 data "linode_profile" "me" {}
 
-resource "linode_instance" "swarm-node" {
-  count            = 1
-  label            = "swarm-node-${count.index}"
+# create manager nodes
+resource "linode_instance" "swarm_manager" {
+  count            = var.MANAGER_COUNT
+  label            = "swarm-manager-${count.index}"
   image            = "linode/ubuntu20.04"
   type             = "g6-nanode-1"
   region           = "eu-west"
-  tags             = ["terraform", "ansible", "docker", "swarm"]
+  tags             = ["terraform", "ansible", "docker", "swarm", "manager"]
   group            = "swarm-cluster"
   private_ip       = true
   root_pass        = var.NODE_ROOT_PWD
   authorized_keys  = [trimspace(file(var.PUBLIC_KEY_PATH))]
   authorized_users = [data.linode_profile.me.username]
+}
+
+# create worker nodes
+resource "linode_instance" "swarm_worker" {
+  count            = var.MANAGER_COUNT
+  label            = "swarm-worker-${count.index}"
+  image            = "linode/ubuntu20.04"
+  type             = "g6-nanode-1"
+  region           = "eu-west"
+  tags             = ["terraform", "ansible", "docker", "swarm", "worker"]
+  group            = "swarm-cluster"
+  private_ip       = true
+  root_pass        = var.NODE_ROOT_PWD
+  authorized_keys  = [trimspace(file(var.PUBLIC_KEY_PATH))]
+  authorized_users = [data.linode_profile.me.username]
+}
+
+# register cluster domain
+resource "linode_domain" "cluster_domain" {
+  type      = "master"
+  domain    = var.DOMAIN_NAME
+  soa_email = data.linode_profile.me.email
+  tags      = ["terrafrom", "swarm"]
+}
+
+# set up dns round robin to manager nodes where traefik will be deployed
+resource "linode_domain_record" "manager_nodes" {
+  count       = var.MANAGER_COUNT
+  domain_id   = linode_domain.cluster_domain.id
+  record_type = "A"
+  name        = "www"
+  port        = 80
+  priority    = 10
+  weight      = 5
+  target      = linode_instance.swarm_manager[count.index].ip_address
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,5 +14,19 @@ variable "PUBLIC_KEY_PATH" {
   description = "public  ssh key that is added to the nodes"
   type      = string
   sensitive = true
+}
 
+variable "MANAGER_COUNT" {
+  description = "count of swarm manager nodes"
+  type      = number
+}
+
+variable "WORKER_COUNT" {
+  description = "count of swarm worker nodes"
+  type      = number
+}
+
+variable "DOMAIN_NAME" {
+  description = "owned domain name"
+  type      = string
 }
